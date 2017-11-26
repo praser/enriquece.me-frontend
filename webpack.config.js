8dcceca0937717df5e97eb653fe0703674367a12
Rubens Praser Júnior <praser@gmail.com>
2017-11-26 20:13:15 -0200

    Change indentation to use 2 spaces

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,18 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
-    devtool: 'inline-source-map',
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Enriquece.me'
-        })
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Enriquece.me'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 }
